249566079d28a69bc3c297659403f0622fc0e495
SELeenux <43295067+SELeenux@users.noreply.github.com>
2020-08-26 18:58:09 +0900

    Add background.js.

---
+++ revised-ver/background.js
@@ -0,0 +1,22 @@
+const body = document.querySelector("body");
+
+const IMG_NB = 6;
+
+function changeBgImg(num) {
+  const file = `./images/${num}.jpg`;
+  body.style.backgroundImage = `url(${file})`;
+  body.style.backgroundSize = "cover";
+  console.dir(body.style);
+}
+
+function genRandNum() {
+  const num = Math.floor(Math.random() * IMG_NB + 1);
+  return (num);
+}
+
+function init() {
+  const randNum = genRandNum();
+  changeBgImg(randNum);
+}
+
+init();
